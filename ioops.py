4cdec1f04a19f1d9f6dfc3ebe7431a77a2c336f4
anty-filidor <ahTrhEBRbbXTdtHKH8VqaiWhX46UHXiae22>
2020-03-21 20:00:08 +0100

    xdxd

--- ioops.py
+++ ioops.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import numpy as np
 import pandas as pd
-
+import scipy
 
 def comp_status(nodes: str) -> pd.DataFrame:
     """
@@ -10,11 +10,48 @@ def comp_status(nodes: str) -> pd.DataFrame:
     :param nodes: name of csv with nodes
     :return: data frame with nodes names and their states
     """
+
+    # read csv to pd Data Frame
     n = pd.read_csv(nodes, header=None)
     n = n.rename(columns={0: 'n'})
     n = n.set_index('n')
+
+    #n = n.sum(axis=1)
+    #n = n / 10
+
+    # convert temperature to catrgorical value
+    n[2] = n[2].apply(lambda x: 2 if x > 38 else (1 if x > 37.5 else 0))
+
+    '''
+    - kaszel - 10
+    - temperatura - float value
+    - dusznosc - 10
+    - zmeczenie - 7
+    - bol_glowy - 7
+    - bol_miesni - 7
+    - bol_gardla - 7
+    - zaburzenie_wechu - 7
+    - zaburzenie_smaku - 7
+    - katar - 2
+    - kichanie - 2
+    - nudnosci - 2
+    - biegunka - 2
+    - bol_brzucha - 2
+    - zawroty_glowy - 2
+    - niepokoj - 2
+    - kolatanie_serca - 2
+    - zime_dreszcze - 2
+    '''
+    vec = [10, 2, 10, 7, 7, 7, 7, 7, 7, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+
+    # compute softmax
+    vec = scipy.special.softmax(vec)
+
+    # set probabilities for each parameter
+    n = n * vec
+
+    # compute overall probability
     n = n.sum(axis=1)
-    n = n / 10
 
     return n
 
--- spreading.py
+++ spreading.py
@@ -5,7 +5,7 @@ import pandas as pd
 from ioops import read_net
 
 
-def sperad(G: nx.Graph, ill_node, visited: list=[]) -> nx.Graph:
+def sperad(G: nx.Graph, ill_node, visited: set = None) -> nx.Graph:
     """
     This method spreads disease from given node to the entire network
     :param G: graph within disease is being spread
@@ -16,7 +16,11 @@ def sperad(G: nx.Graph, ill_node, visited: list=[]) -> nx.Graph:
 
     # set status of ill node as 100% infected
     G.nodes[ill_node]['w'] = 1
-    visited.append(ill_node)
+
+    # update visited
+    if visited is None:
+        visited = set()
+    visited.add(ill_node)
 
     # check if neighbour nodes are in taboo list
     nbrs = [*G[ill_node].keys()]
@@ -26,20 +30,43 @@ def sperad(G: nx.Graph, ill_node, visited: list=[]) -> nx.Graph:
     # take it's fresh neighbours and iterate through them
     for n in nbrs:
 
+        # if node is already infected go over it
+        if G.nodes[n]['w'] == 1:
+            continue
+
+        # take neighbours of node
+        _n = [*G[n].keys()]
+
+        # take probabilities from neighbouring nodes and mul it by connection
+        # weights
+        probs = [G[__n][n]['w'] * G.nodes[__n]['w']for __n in _n]
+        probs = np.array(probs)
+
+        '''
         # read weight of connection
         w = G[n][ill_node]
         # print(n, w, G.nodes[n])
 
         # compute probability of being affected
         prob = w['w'] * G.nodes[n]['w']
+        print(prob)
 
         # toss a coin with binomial prob and check if node will become ill
         toss = np.random.choice(np.arange(0, 2), p=[1 - prob, prob])
+        '''
+
+        prob = probs * G.nodes[n]['w']
+        prob = 1 - prob
+        prob = np.prod(prob)
+        # prob is probability of being health of node "n"
+
+        # toss a coin with binomial prob and check if node will become ill
+        toss = np.random.choice(np.arange(0, 2), p=[prob, 1 - prob])
 
         # if toss is 1 then node is ill
         if toss == 1:
             # append it to the taboo list
-            visited.append(n)
+            visited.add(n)
             sperad(G, n, visited)
             # print('tango down', n)
 
